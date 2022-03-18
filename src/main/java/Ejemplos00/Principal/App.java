f2f43276a8c129fdfc0623081697ff4aaca29d75
santiagobp24 <santiago83iti@gmail.com>
2022-03-17 22:24:49 -0500

    primer commit

---
+++ src/main/java/Ejemplos00/Principal/App.java
@@ -0,0 +1,13 @@
+package Ejemplos00.Principal;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/Ejemplos00/Principal/Objeto.java
@@ -0,0 +1,11 @@
+package Ejemplos00.Principal;
+
+public class Objeto {
+	
+	public static void main (String[] args) 
+	{
+		int a = 9;
+		System.out.println(a+a);
+	}
+
+}
